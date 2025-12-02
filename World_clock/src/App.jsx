6f1b617be3d63043cf60a495d446584e16493bb4
Vinayak patil <vinayakpatil2103@gmail.com>
2025-12-02 11:45:24 +0530

    Add files via upload

---
+++ World_clock/src/App.jsx
@@ -0,0 +1,14 @@
+import ClockHeading from "./components/ClockHeading";
+import ClockSlogan from "./components/ClockSlogan";
+import CurrentTime from "./components/CurrentTime";
+function App() {
+  return (
+    <div>
+      <ClockHeading></ClockHeading>
+      <ClockSlogan></ClockSlogan>
+      <CurrentTime></CurrentTime>
+    </div>
+  );
+}
+
+export default App;
